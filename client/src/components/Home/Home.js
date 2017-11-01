09b2ba09554d7ca686e3420685360716e164088b
smpr <gcbrown1116@gmail.com>
2017-11-01 11:42:54 -0400

    Dependencies installed and home route setup

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                VagaBond Project
+            </div>
+        );
+    }
+}
+
+export default HomePage;
