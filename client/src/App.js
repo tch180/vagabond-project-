09df1aadda2918bb363e7eb90fc5dc3f825761f9
smpr <gcbrown1116@gmail.com>
2017-11-01 12:59:36 -0400

    ready for commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 import Home from './components/Home/Home'
 import NavBar from './components/BoilerPlate/NavBar'
-import Header from './components/BoilerPlate/Header'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Header />
+
           <NavBar />
           <Switch>
             <Route exact path="/" component={Home} />
