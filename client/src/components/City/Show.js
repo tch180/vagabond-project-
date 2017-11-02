7b84639e364824ebb51fb14c7be84b0a766b3d6e
smpr <gcbrown1116@gmail.com>
2017-11-02 16:42:06 -0400

    MORE

--- client/src/components/City/Show.js
+++ client/src/components/City/Show.js
@@ -20,7 +20,7 @@ const Container = styled.div`
 display: flex;
 flex-direction: column;
 background-color: #233237;
-height: 90vh;
+
 
 `
 const HeaderContainer = styled.div`
@@ -33,6 +33,16 @@ img {
 max-width: 100%;
 height: 500px;
 box-shadow: 3px 3px 10px #99864A;
+}
+`
+const AddPost = styled.div`
+background: #984343;
+padding: 15px;
+margin: 5px;
+max-width: 25vw;
+color: white;
+a {
+text-decoration: none;
 }
 `
 const PostContainer = styled.div`
@@ -67,27 +77,27 @@ class CityShow extends Component {
             body: ''
         }
     }
-    
+
     async componentWillMount() {
-     this.getCityInfo()
-        
+        this.getCityInfo()
+
     }
     getCityInfo = async () => {
         try {
             const cityId = this.props.match.params.cityId
-            
+
             const res = await axios.get(`/api/cities/${cityId}`)
-            this.setState({ cities: res.data})
-        }catch(error){
+            this.setState({ cities: res.data })
+        } catch (error) {
             console.log(error)
         }
     }
 
-    handleUpdateChange = (event) =>{
+    handleUpdateChange = (event) => {
         const attribute = event.target.name
-        const updatePost = {...this.state.currentPost}
+        const updatePost = { ...this.state.currentPost }
         updatePost[attribute] = event.target.value
-        this.setState({currentPost: updatePost })
+        this.setState({ currentPost: updatePost })
     }
 
     handleUpdateSubmit = async (postId) => {
@@ -96,11 +106,11 @@ class CityShow extends Component {
             post: this.state.currentPost
         })
         const city = res.data
-        this.setState({cities: city})
+        this.setState({ cities: city })
     }
 
     toggleSwitch = () => {
-        this.setState({togglePostView: !this.state.togglePostView})
+        this.setState({ togglePostView: !this.state.togglePostView })
     }
 
 
@@ -109,44 +119,45 @@ class CityShow extends Component {
         console.log(this.props.match.params.cityId)
         const res = await axios.delete(`/api/cities/${cityId}/posts/${postId}`)
         const city = res.data
-        this.setState({cities: city})
+        this.setState({ cities: city })
     }
-    
+
     showPost = (index) => {
         this.toggleSwitch()
         const post = this.state.cities.posts[index]
-        this.setState({currentPost: post})
+        this.setState({ currentPost: post })
     }
 
     render() {
         const postView = this.state.togglePostView ?
-        <ShowPost post={this.state.currentPost} 
-        deletePost={this.deletePost}
-        toggleSwitch={this.toggleSwitch} 
-        handleUpdateChange={this.handleUpdateChange}
-        handleUpdateSubmit={this.handleUpdateSubmit} /> :
-        <PostContainer>
-            {/* <Link to="/city/${}" */}
-            {this.state.cities.posts.map((post, index) => {
-                return( 
-                <PostBlock key={index} onClick={() => this.showPost(index)}>
-                    <div><b>{post.title}</b></div>
-                </PostBlock>)
-            })}
-            
-            
-        </PostContainer>
-        
+            <ShowPost post={this.state.currentPost}
+                deletePost={this.deletePost}
+                toggleSwitch={this.toggleSwitch}
+                handleUpdateChange={this.handleUpdateChange}
+                handleUpdateSubmit={this.handleUpdateSubmit} /> :
+            <PostContainer>
+                {/* <Link to="/city/${}" */}
+                {this.state.cities.posts.map((post, index) => {
+                    return (
+                        <PostBlock key={index} onClick={() => this.showPost(index)}>
+                            <div><b>{post.title}</b></div>
+                        </PostBlock>)
+                })}
+
+
+            </PostContainer>
+
         return (
             <Container>
                 <HeaderContainer>
-                <InfoBar>City: {this.state.cities.name} Region: {this.state.cities.region} Country: {this.state.cities.country}</InfoBar>
+                    <InfoBar>City: {this.state.cities.name} Region: {this.state.cities.region} Country: {this.state.cities.country}</InfoBar>
                     <div><img src={this.state.cities.image} /></div><br />
-                   
+
                 </HeaderContainer>
-                
-                <div>Add Post</div>
-                {postView}
+                <div>
+                    <center><Link to={`/city/${this.props.match.params.cityId}/NewPost`}><AddPost>Add Post</AddPost></Link></center>
+                    {postView}
+               </div>
             </Container>
         );
     }
