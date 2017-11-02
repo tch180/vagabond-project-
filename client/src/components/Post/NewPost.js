ce73d0d1ca3427cf200ec5d3729e67dff666f038
tch180 <thopstadredner@gmail.com>
2017-11-02 15:58:21 -0400

    New post styling and layout down

--- client/src/components/Post/NewPost.js
+++ client/src/components/Post/NewPost.js
@@ -6,7 +6,46 @@ import { Redirect } from 'react-router-dom'
 
 const PostContainer = styled.div`
 display: flex;
+text-align:center;
+justify-content: center;
+background-color: #233237;
+color: white;
+border: 1px solid grey;
+height: 90vh;
 `
+
+const NewPostContainer = styled.div `
+background: #984343;
+border: 1px solid grey;
+margin: 40px 120px;
+box-shadow: 3px 3px 10px #99864A;
+width: 35vw;
+height: 45vh;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+
+button {
+    color: black;
+    background:white;
+}
+
+div {
+    padding: 10px 0px;
+    /* margin: 10px; */
+}
+textarea {
+    width: 25vw;
+    height: 15vh;
+
+`
+
+
+
+
+
+
 class NewPost extends Component {
     state = {
         post: {
@@ -33,26 +72,38 @@ class NewPost extends Component {
         render() {
             if (this.state.redirectToPost) {return <Redirect to={`/city/${this.props.match.params.cityId}`} />}
         return (
+            
             <PostContainer>
-                <div>New Post</div>
+                <NewPostContainer>
+
+                <div><h2>New Post</h2></div>
+
+                
                 <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label htmlFor="title">Title</label>
-                        <input
-                            onChange={this.handleChange} name="title"
-                            type="text" value={this.state.post.title}
-                        />
-                    </div>
-                    <div>
-                        <label >Body</label>
-                         
-                            <textarea  value={this.state.post.body} htmlFor="body"
-                            name="body" onChange={this.handleChange}/>
+
+                    <div><input placeholder='Title of your post' onChange={this.handleChange} name="title"
+                type="text" value={this.state.post.title} /></div>
+                       
+         <div>   <input placeholder='Ratings' className="rating"
+                            onChange={this.handleChange} name="rating"
+                            type="number" max='10' min='1' value={this.state.post.rating}/> </div>
+                    
+                       
+          <div>  <input placeholder='Picture'
+                            onChange={this.handleChange} name="image"
+                            type="text" value={this.state.post.image}/></div>
+                  
+                
+              <div>
+             <textarea  value={this.state.post.body} htmlFor="body"
+                           placeholder="Please enter your Review" name="body" onChange={this.handleChange}/></div>
                            
-                    </div>
+                    
                     
                     <button>Post!</button>
                 </form>
+              
+                </NewPostContainer>
             </PostContainer>
         );
     }
