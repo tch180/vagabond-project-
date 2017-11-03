b5bc76d11a41c3f5ff3fc53e1e04601cadd6a8c4
SadEvilPenguin <mears.chris@gmail.com>
2017-11-02 21:31:35 -0400

    update post view

--- client/src/components/Post/ShowPost.js
+++ client/src/components/Post/ShowPost.js
@@ -6,10 +6,34 @@ const PostContainer = styled.div `
     background: #984343;
     margin: 20px 150px;
     padding-top: 15px;
+    box-shadow: 3px 3px 10px grey;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
+    button {
+    background: #EAC67A;
+    color: white;
+    font-weight: bolder;
+    text-decoration: none;
+    text-align: cneter;
+    letter-spacing: .1em;
+    font-size: 1rem;
+    border: none;
+    margin: 5px;
+    border-radius: 2px;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 2rem;
+    text-transform: uppercase;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14),
+    0 1px 5px 0 rgba(0, 0, 0, .12),
+    0 3px 1px -2px rgba(0, 0, 0, .12);
+    &:hover {
+        background: rgb(198, 165, 103);
+    }
+}
 `
 
 const PostContent = styled.div `
@@ -23,10 +47,23 @@ const PostContent = styled.div `
     img {
         width: 350px;
     }
+    h1 {
+        margin: 0;
+    }
+    div {
+        border: 2px solid white;
+        width: 90%;
+        text-align: center;
+        margin: 10px;
+        padding: 10px;
+    }
+    p {
+        text-align: left;
+    }
     
 `
 
-const NewPostContainer = styled.div `
+const UpdatePostContainer = styled.div `
 margin: 40px 120px;
 padding: 40px;
 display: flex;
@@ -34,10 +71,6 @@ justify-content: center;
 align-items: center;
 flex-direction: column;
 
-button {
-    color: black;
-    background:white;
-}
 
 input {
     margin: 10px 0px;
@@ -96,16 +129,20 @@ class ShowPost extends Component {
         </div>
 
         const post = <PostContent>
+            <div>
             <h1>{this.props.post.title}</h1>
+            </div>
+            <div>
             <img src={this.props.post.image} alt={this.props.post.title} />
             <p>{this.props.post.body}</p>
+            </div>
         </PostContent>
-        const updateView = <NewPostContainer>
+        const updateView = <UpdatePostContainer>
             <input name="title" type="text" value={this.props.post.title} onChange={this.handleChange} />
             <input name="image" type="text" value={this.props.post.image} onChange={this.handleChange} />
             <input name="rating" type="number" className='rating' value={this.props.post.rating} onChange={this.handleChange} />
             <textarea value={this.props.post.body} htmlFor="body" name="body" onChange={this.handleChange} />
-        </NewPostContainer>
+        </UpdatePostContainer>
         const postView = this.state.toggleUpdate
             ? updateView
             : this.state.toggleDelete ? deleteConfirmation : post
