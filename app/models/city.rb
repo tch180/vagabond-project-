7e7650b5ad9fdf2735b72489592a279ac1c8a655
smpr <gcbrown1116@gmail.com>
2017-11-03 09:57:33 -0400

    city validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,7 @@
 class City < ApplicationRecord
-  has_many :posts, dependent: :destroy
+  has_many :posts, dependent: :destroy 
+  validates :name, presence: true 
+  validates :region, presence: true 
+  validates :country, presence: true
+  validates :image, presence: true
 end
